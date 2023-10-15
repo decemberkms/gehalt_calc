e68ab5b1613764393fc36a34411449ba854d6145
Min <decemberkms@gmail.com>
2023-10-15 16:18:46 +0200

    Basic code

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: const Text('My'),
+            ),
+            const Text(
+              'Hello World',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
